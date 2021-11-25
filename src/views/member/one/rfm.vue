156537d1603110b65bab0009d8812d03689f6ad9
lixiang <lixiangyuanyang@163.com>
2021-11-25 16:49:33 +0800

    上传代码

---
+++ src/views/member/one/rfm.vue
@@ -0,0 +1,342 @@
+<template>
+    <div class="app-container">
+        <SearchForm :form-options="formOptions">
+            <template #handleBtn>
+                <el-button
+                    type="primary"
+                    icon="el-icon-search"
+                    size="mini"
+                >查询
+                </el-button>
+                <el-button type="default" size="mini">重置</el-button>
+            </template>
+        </SearchForm>
+        <div class="app-container__body">
+            <div class="app-container__body-table">
+                <CustomTable :columns="columns" :table-data='tableData'>
+                    <template #handle="slotProps">
+                        <el-button type="default" size="mini" icon="el-icon-search" @click="handleEdit(slotProps.row)">
+                            查看
+                        </el-button>
+                        <el-button type="danger" size="mini" icon="el-icon-delete" @click="handleDelete(slotProps.row)">
+                            删除
+                        </el-button>
+                    </template>
+                </CustomTable>
+            </div>
+        </div>
+        <el-pagination
+            class="table-pagination"
+            :current-page="pagination.currentPage"
+            :page-sizes="[5, 10, 15, 20]"
+            :page-size="pagination.pageSize"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="pagination.total"
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+        />
+    </div>
+</template>
+
+<script>
+import CustomTable from '@/components/customTable';
+import SearchForm from '@/components/seachForm';
+
+export default {
+    name: 'BrandInformation',
+    components: {
+        SearchForm,
+        CustomTable
+    },
+    data() {
+        return {
+            columns: [
+                {
+                    prop: 'code',
+                    label: '会员卡号',
+                    sortable: true
+                },
+                {
+                    prop: 'name',
+                    label: '姓名'
+                },
+                {
+                    prop: 'concat',
+                    label: '电话'
+                },
+                {
+                    prop: 'mobile',
+                    label: '会员等级'
+                },
+                {
+                    prop: 'mode',
+                    label: '开卡门店'
+                },
+                {
+                    prop: 'province',
+                    label: '当前积分'
+                },
+                {
+                    prop: 'province',
+                    label: '当前余额'
+                },
+                {
+                    prop: 'province',
+                    label: '订单总计'
+                },
+                {
+                    prop: 'province',
+                    label: '订单单价'
+                },
+                {
+                    prop: 'province',
+                    label: '消费次数'
+                },
+                {
+                    prop: 'province',
+                    label: '最近消费日期'
+                },
+                {
+                    prop: 'province',
+                    label: '未消费天数'
+                }
+            ],
+            tableData: [
+                {
+                    code: '001',
+                    name: '郑州批发城',
+                    concat: '小郑',
+                    mobile: '1355454623',
+                    mode: '购销',
+                    province: '河南',
+                    city: '郑州',
+                    address: '河南省郑州市中原区',
+                    salesman: '小明',
+                    phone: '13183117895',
+                    createTime: '1636727628063',
+                    updateTime: '1636727628063'
+                },
+                {
+                    code: '002',
+                    name: '郑州批发城',
+                    concat: '小郑',
+                    mobile: '1355454623',
+                    mode: '购销',
+                    province: '河南',
+                    city: '郑州',
+                    address: '河南省郑州市中原区',
+                    salesman: '小明',
+                    phone: '13183117895',
+                    createTime: '1636727628063',
+                    updateTime: '1636727628063'
+                },
+                {
+                    code: '003',
+                    name: '郑州批发城',
+                    concat: '小郑',
+                    mobile: '1355454623',
+                    mode: '购销',
+                    province: '河南',
+                    city: '郑州',
+                    address: '河南省郑州市中原区',
+                    salesman: '小明',
+                    phone: '13183117895',
+                    createTime: '1636727628063',
+                    updateTime: '1636727628063'
+                },
+                {
+                    code: '004',
+                    name: '郑州批发城',
+                    concat: '小郑',
+                    mobile: '1355454623',
+                    mode: '购销',
+                    province: '河南',
+                    city: '郑州',
+                    address: '河南省郑州市中原区',
+                    salesman: '小明',
+                    phone: '13183117895',
+                    createTime: '1636727628063',
+                    updateTime: '1636727628063'
+                },
+                {
+                    code: '005',
+                    name: '郑州批发城',
+                    concat: '小郑',
+                    mobile: '1355454623',
+                    mode: '购销',
+                    province: '河南',
+                    city: '郑州',
+                    address: '河南省郑州市中原区',
+                    salesman: '小明',
+                    phone: '13183117895',
+                    createTime: '1636727628063',
+                    updateTime: '1636727628063'
+                }
+            ],
+            pagination: {
+                currentPage: 1,
+                pageSize: 10,
+                total: 20
+            },
+            formOptions: [
+                {
+                    label: '关键词',
+                    prop: 'keyWord',
+                    element: 'el-input',
+                    initValue: undefined,
+                    placeholder: '商品条码/商品名称',
+                    clearable: true
+                },
+                {
+                    label: '门店',
+                    prop: 'store',
+                    element: 'el-select',
+                    initValue: undefined,
+                    placeholder: '选择分类',
+                    clearable: true,
+                    options: [
+                        {
+                            label: '管锐技术测试总部',
+                            value: '1'
+                        },
+                        {
+                            label: '管锐技术测试分店',
+                            value: '2'
+                        }
+                    ]
+                },
+                {
+                    label: '会员类别',
+                    prop: 'type',
+                    element: 'el-select',
+                    initValue: undefined,
+                    placeholder: '类别选择',
+                    clearable: true,
+                    options: [
+                        {
+                            label: '微会员',
+                            value: '1'
+                        },
+                        {
+                            label: '普通会员',
+                            value: '3'
+                        },
+                        {
+                            label: '白银会员',
+                            value: '3'
+                        }
+                    ]
+                },
+                {
+                    label: '客户性别',
+                    prop: 'sex',
+                    element: 'el-select',
+                    initValue: undefined,
+                    placeholder: '类别选择',
+                    clearable: true,
+                    options: [
+                        {
+                            label: '男',
+                            value: '1'
+                        },
+                        {
+                            label: '女',
+                            value: '2'
+                        },
+                        {
+                            label: '未知',
+                            value: '3'
+                        }
+                    ]
+                }
+            ]
+        };
+    },
+
+    methods: {
+        /**
+         * 点击顶部tab导航栏
+         * @param e
+         */
+        handleClick(e) {
+
+        },
+        /**
+         * 分页控制每页多少条
+         */
+        handleSizeChange() {
+            console.log(11111);
+        },
+        /**
+         * 分页控制第几页
+         */
+        handleCurrentChange() {
+            console.log(2222);
+        },
+        handleDelete(data) {
+
+        },
+        handleEdit(data) {
+
+        },
+        // 校验
+        onValidate(callback) {
+            this.$refs.formRef.validate((valid) => {
+                if (valid) {
+                    callback();
+                } else {
+                    return false;
+                }
+            });
+        },
+        // 搜索
+        onSearch() {
+            this.onValidate(() => {
+                this.$emit('onSearch', this.formData);
+            });
+        },
+        // 导出
+        onExport() {
+            this.onValidate(() => {
+                this.$emit('onExport', this.formData);
+            });
+        },
+        onReset() {
+            this.$refs.formRef.resetFields();
+        },
+        // 添加初始值
+        addInitValue() {
+            const obj = {};
+            this.formOptions.forEach((curr) => {
+                if (curr.initValue !== undefined) {
+                    obj[curr.prop] = curr.initValue;
+                }
+            });
+            this.formData = obj;
+        }
+    }
+};
+</script>
+<style lang="scss" scoped>
+.app-container {
+    height: 100%;
+
+    .handle-table-btn {
+        padding: 4px;
+        font-size: 12px;
+    }
+
+    .table-pagination {
+        margin-top: 30px;
+    }
+
+    &__body {
+        height: calc(100% - 200px);
+    }
+
+    &__body-table {
+        height: 100%;
+    }
+}
+</style>
+
