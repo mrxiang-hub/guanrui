b050a7f5e019e1485d5d7183dabcc8edcbe2dea2
lixiang <lixiangyuanyang@163.com>
2021-12-11 21:34:14 +0800

    安装工具

--- src/views/setting/software/tools.vue
+++ src/views/setting/software/tools.vue
@@ -5,53 +5,32 @@
                 <div slot="header" class="clearfix">
                     <span>安装.Net Framework 4.0</span>
                 </div>
-                <div class="app-container__body">
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">软件名称</div>
-                        <div class="app-container__part-value">Net Framework 4.0</div>
-                    </div>
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">软件大小</div>
-                        <div class="app-container__part-value">48.11MB</div>
-                    </div>
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">更新时间</div>
-                        <div class="app-container__part-value">2018/10/12</div>
-                    </div>
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">运行环境</div>
-                        <div class="app-container__part-value">Windows XP/Windows 7/Windows 8/Windows 10</div>
-                    </div>
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">安装下载</div>
-                        <div class="app-container__part-value">点击下载</div>
-                    </div>
-                </div>
-            </el-card>
-            <el-card class="app-container__cards-item">
-                <div slot="header" class="clearfix">
-                    <span>安装.Net Framework 4.0</span>
-                </div>
-                <div class="app-container__body">
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">软件名称</div>
-                        <div class="app-container__part-value">Net Framework 4.0</div>
-                    </div>
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">软件大小</div>
-                        <div class="app-container__part-value">48.11MB</div>
-                    </div>
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">更新时间</div>
-                        <div class="app-container__part-value">2018/10/12</div>
-                    </div>
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">运行环境</div>
-                        <div class="app-container__part-value">Windows XP/Windows 7/Windows 8/Windows 10</div>
-                    </div>
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">安装下载</div>
-                        <div class="app-container__part-value">点击下载</div>
+                <div class="app-container__wrap">
+                    <el-image
+                        class="app-container__image"
+                        src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+                        fit="fit"></el-image>
+                    <div class="app-container__body">
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">软件名称</div>
+                            <div class="app-container__part-value">Net Framework 4.0</div>
+                        </div>
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">软件大小</div>
+                            <div class="app-container__part-value">48.11MB</div>
+                        </div>
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">更新时间</div>
+                            <div class="app-container__part-value">2018/10/12</div>
+                        </div>
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">运行环境</div>
+                            <div class="app-container__part-value">Windows XP/Windows 7/Windows 8/Windows 10</div>
+                        </div>
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">安装下载</div>
+                            <div class="app-container__part-value">点击下载</div>
+                        </div>
                     </div>
                 </div>
             </el-card>
@@ -59,53 +38,32 @@
                 <div slot="header" class="clearfix">
                     <span>安装.Net Framework 4.0</span>
                 </div>
-                <div class="app-container__body">
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">软件名称</div>
-                        <div class="app-container__part-value">Net Framework 4.0</div>
-                    </div>
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">软件大小</div>
-                        <div class="app-container__part-value">48.11MB</div>
-                    </div>
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">更新时间</div>
-                        <div class="app-container__part-value">2018/10/12</div>
-                    </div>
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">运行环境</div>
-                        <div class="app-container__part-value">Windows XP/Windows 7/Windows 8/Windows 10</div>
-                    </div>
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">安装下载</div>
-                        <div class="app-container__part-value">点击下载</div>
-                    </div>
-                </div>
-            </el-card>
-            <el-card class="app-container__cards-item">
-                <div slot="header" class="clearfix">
-                    <span>安装.Net Framework 4.0</span>
-                </div>
-                <div class="app-container__body">
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">软件名称</div>
-                        <div class="app-container__part-value">Net Framework 4.0</div>
-                    </div>
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">软件大小</div>
-                        <div class="app-container__part-value">48.11MB</div>
-                    </div>
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">更新时间</div>
-                        <div class="app-container__part-value">2018/10/12</div>
-                    </div>
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">运行环境</div>
-                        <div class="app-container__part-value">Windows XP/Windows 7/Windows 8/Windows 10</div>
-                    </div>
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">安装下载</div>
-                        <div class="app-container__part-value">点击下载</div>
+                <div class="app-container__wrap">
+                    <el-image
+                        class="app-container__image"
+                        src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+                        fit="fit"></el-image>
+                    <div class="app-container__body">
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">软件名称</div>
+                            <div class="app-container__part-value">Net Framework 4.0</div>
+                        </div>
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">软件大小</div>
+                            <div class="app-container__part-value">48.11MB</div>
+                        </div>
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">更新时间</div>
+                            <div class="app-container__part-value">2018/10/12</div>
+                        </div>
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">运行环境</div>
+                            <div class="app-container__part-value">Windows XP/Windows 7/Windows 8/Windows 10</div>
+                        </div>
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">安装下载</div>
+                            <div class="app-container__part-value">点击下载</div>
+                        </div>
                     </div>
                 </div>
             </el-card>
@@ -113,26 +71,98 @@
                 <div slot="header" class="clearfix">
                     <span>安装.Net Framework 4.0</span>
                 </div>
-                <div class="app-container__body">
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">软件名称</div>
-                        <div class="app-container__part-value">Net Framework 4.0</div>
+                <div class="app-container__wrap">
+                    <el-image
+                        class="app-container__image"
+                        src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+                        fit="fit"></el-image>
+                    <div class="app-container__body">
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">软件名称</div>
+                            <div class="app-container__part-value">Net Framework 4.0</div>
+                        </div>
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">软件大小</div>
+                            <div class="app-container__part-value">48.11MB</div>
+                        </div>
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">更新时间</div>
+                            <div class="app-container__part-value">2018/10/12</div>
+                        </div>
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">运行环境</div>
+                            <div class="app-container__part-value">Windows XP/Windows 7/Windows 8/Windows 10</div>
+                        </div>
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">安装下载</div>
+                            <div class="app-container__part-value">点击下载</div>
+                        </div>
                     </div>
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">软件大小</div>
-                        <div class="app-container__part-value">48.11MB</div>
+                </div>
+            </el-card>
+            <el-card class="app-container__cards-item">
+                <div slot="header" class="clearfix">
+                    <span>安装.Net Framework 4.0</span>
+                </div>
+                <div class="app-container__wrap">
+                    <el-image
+                        class="app-container__image"
+                        src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+                        fit="fit"></el-image>
+                    <div class="app-container__body">
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">软件名称</div>
+                            <div class="app-container__part-value">Net Framework 4.0</div>
+                        </div>
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">软件大小</div>
+                            <div class="app-container__part-value">48.11MB</div>
+                        </div>
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">更新时间</div>
+                            <div class="app-container__part-value">2018/10/12</div>
+                        </div>
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">运行环境</div>
+                            <div class="app-container__part-value">Windows XP/Windows 7/Windows 8/Windows 10</div>
+                        </div>
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">安装下载</div>
+                            <div class="app-container__part-value">点击下载</div>
+                        </div>
                     </div>
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">更新时间</div>
-                        <div class="app-container__part-value">2018/10/12</div>
-                    </div>
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">运行环境</div>
-                        <div class="app-container__part-value">Windows XP/Windows 7/Windows 8/Windows 10</div>
-                    </div>
-                    <div class="app-container__part">
-                        <div class="app-container__part-label">安装下载</div>
-                        <div class="app-container__part-value">点击下载</div>
+                </div>
+            </el-card>
+            <el-card class="app-container__cards-item">
+                <div slot="header" class="clearfix">
+                    <span>安装.Net Framework 4.0</span>
+                </div>
+                <div class="app-container__wrap">
+                    <el-image
+                        class="app-container__image"
+                        src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+                        fit="fit"></el-image>
+                    <div class="app-container__body">
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">软件名称</div>
+                            <div class="app-container__part-value">Net Framework 4.0</div>
+                        </div>
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">软件大小</div>
+                            <div class="app-container__part-value">48.11MB</div>
+                        </div>
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">更新时间</div>
+                            <div class="app-container__part-value">2018/10/12</div>
+                        </div>
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">运行环境</div>
+                            <div class="app-container__part-value">Windows XP/Windows 7/Windows 8/Windows 10</div>
+                        </div>
+                        <div class="app-container__part">
+                            <div class="app-container__part-label">安装下载</div>
+                            <div class="app-container__part-value">点击下载</div>
+                        </div>
                     </div>
                 </div>
             </el-card>
@@ -151,8 +181,22 @@ export default {
     &__cards {
         overflow-y: auto;
     }
+
     &__cards-item {
         margin-bottom: 20px;
+    }
+
+    &__wrap {
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+    }
+
+    &__image {
+        width: 150px;
+        height: 150px;
+        border-radius: 16px;
+        margin-right: 200px;
     }
 
     &__body {
@@ -165,6 +209,7 @@ export default {
         justify-content: flex-start;
         align-items: center;
         font-size: 14px;
+
     }
 
     &__part-label {
@@ -175,5 +220,8 @@ export default {
 
     }
 
+    ::v-deep .el-scrollbar__wrap {
+        overflow: auto;
+    }
 }
 </style>
