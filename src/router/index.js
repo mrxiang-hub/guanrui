1bbb94628bb01202ef68501ff5647d9802653028
lixiang <lixiangyuanyang@163.com>
2021-12-11 18:30:06 +0800

    删除无用菜单

--- src/router/index.js
+++ src/router/index.js
@@ -443,19 +443,7 @@ export const constantRoutes = [
             }
         ]
     },
-
-    {
-        path: 'external-link',
-        component: Layout,
-        children: [
-            {
-                path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-                meta: { title: 'External Link', icon: 'link' }
-            }
-        ]
-    },
-
-    // 404 page must be placed at the end !!!
+    // 404
     { path: '*', redirect: '/404', hidden: true }
 ];
 
